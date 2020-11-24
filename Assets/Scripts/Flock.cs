cb21d0bfc69548f1addfb6f426aeb9815e977dac
Khoa Dang Hoang <konadagoha@gmail.com>
2020-11-24 17:26:48 -0500

    Initialize Unity Project; Add Flock, FlockAgent, and FlockBehavior scripts

---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockAgent agentPrefab;
+    public FlockBehavior behavior;
+
+    [Range(10, 500)]
+    public int startingCount = 250;
+    [Range(1f, 100f)]
+    public float driveFactor = 10;
+    [Range(1f, 100f)]
+    public float maxSpeed = 5f;
+    [Range(1f, 10f)]
+    public float neighborRadius = 1.5f;
+    [Range(0f, 1f)]
+    public float avoidanceRadiusMultiplier = 0.5f;
+
+    private List<FlockAgent> agents = new List<FlockAgent>();
+    private const float agentDensity = 0.08f;
+
+    private float squareMaxSpeed;
+    private float squareNeighborRadius;
+    private float squareAvoidanceRadius;
+
+    public float SquareAvoidanceRadius { get { return squareAvoidanceRadius; } }
+
+    private void Start()
+    {
+        squareMaxSpeed = maxSpeed * maxSpeed;
+        squareNeighborRadius = neighborRadius * neighborRadius;
+        squareAvoidanceRadius = squareNeighborRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
+
+        for (int i = 0; i < startingCount; i++)
+        {
+            FlockAgent newAgent = Instantiate(
+                agentPrefab,
+                Random.insideUnitCircle * startingCount * agentDensity,
+                Quaternion.Euler(Vector3.forward * Random.Range(0, 360)),
+                transform
+            );
+            newAgent.name = "Agent_" + i;
+            agents.Add(newAgent);
+        }
+    }
+}
